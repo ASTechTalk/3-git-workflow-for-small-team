785eef58534940e71f5b8c89dea3af52108297ca
ksoichiro <soichiro.kashima@gmail.com>
2014-06-23 00:01:02 +0900

    Added build program to create reveal.js slides.

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-INPUT=GitWorkflowForSmallTeam.md
-OUTPUT=slide.html
 PANDOC="docker run -t -i -v `pwd`:/workspace ksoichiro/pandoc pandoc"
+INPUT=GitWorkflowForSmallTeam.md
 
+# Reveal.js
+cp -pR config/reveal.js ./
+pushd reveal.js > /dev/null
+grunt themes
+popd > /dev/null
+
+# Pandoc
+OUTPUT=slide.html
 $PANDOC \
   -s \
   -t revealjs \
-  -i \
-  -V theme:simple \
+  -V theme:default \
   -V transition:linear \
   -o $OUTPUT \
   $INPUT
